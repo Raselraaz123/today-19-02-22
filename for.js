390421b4eabd1cff28d2a17cff15d3566744e14d
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-19 22:59:38 +0600

    add

---
+++ for.js
@@ -0,0 +1,15 @@
+const paragarap = "lotFuga quasi quod atque\n" +
+    " numquam soluta ipsa, porro praesentium\n" +
+    "repellendus.Fuga quasi quod atque\n " +
+    "numquam soluta ipsa, porro praesentium repellendus."
+console.log(paragarap);
+
+const newpara = "Fuga quasi quatque numquam soluta ipsa, porro praesentium \n"
+"repellendus.Fuga quasi quod atque numquam soluta ipsa, porro praesentium\n"
+"repellendus.Fuga quasi quod atque numquam soluta ipsa, porro praesentium\n"
+"repellendus.Fuga quasi quod atque numquam soluta ipsa, porro praesentium\n"
+"repellendus.Fuga quasi quod atque numquam soluta ipsa, porro praesentium\n"
+"repellendus.Fuga quasi quod atque numquam soluta ipsa, porro praesentium\n"
+"repellendus.Fuga quasi quod atque numquam soluta ipsa, porro praesentium\n"
+"repellendus."
+console.log(newpara)
---
+++ three.js
@@ -0,0 +1,16 @@
+function addNumber(num1, num2 =20) {
+    const add = num1 + num2;
+    
+   return add
+}
+const total = addNumber(10)
+console.log(total)
+
+
+function add(num1, num2 = 33) {
+    
+    const total = num1 + num2;
+    return total;
+}
+const result = add(15);
+console.log(result);
