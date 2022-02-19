8d27916702a623a0de1dcd71c52e121790886d68
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-19 20:26:10 +0600

    third commit

--- one.js
+++ one.js
@@ -1,7 +1,66 @@
-// var name = 'rasel'
-// name = 'roman'
-// console.log(name)
-// let number = 50;
-// number = 150;
-console.log('rasel');
-console.log("Roman")
+var myName = 'Rasel';
+// console.log(myName);
+myName = 'Rasel Raaz'
+console.log(myName);
+let number = 500;
+console.log(number);
+number = 1000;
+console.log(number)
+const maa = 'Rabaya';
+console.log(maa);
+// let maa = 'Rabayas'
+// console.log(maa);
+if (number > 0) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+if (100 < 30) {
+    console.log(100, "is lass than 30")
+}
+else {
+    console.log(100, 'is biger than 30');
+}
+if (number == 1000) {
+    console.log(true)
+}
+else {
+    console.log(false);
+}
+if (number != 1000) {
+    console.log(true)
+}
+else {
+    console.log( false )
+}
+if (number != 700) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+if (number >= 1004) {
+    console.log("rasel")
+}
+else {
+    console.log('Roman')
+}
+if (number >= 900) {
+    console.log('rabaya')
+}
+else {
+    console.log('other')
+}
+if (number <= 300) {
+    console.log(true)
+}
+else {
+    console.log("game over")
+}
+if (number <= 20000) {
+    console.log("is true game start")
+}
+else {
+    console.log(false,'tring')
+}
